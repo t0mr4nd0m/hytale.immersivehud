634995586baba90ca9c57903b8d1006a0fa8e301
T0mR4nD0m <t0mr4nd0m@gmail.com>
2026-04-16 13:18:03 +0200

    refactor: variable name hintDelayMsHint to currentHideDelayMs

--- src/main/java/com/tom/immersivehudplugin/runtime/PlayerHudState.java
+++ src/main/java/com/tom/immersivehudplugin/runtime/PlayerHudState.java
@@ -29,7 +29,7 @@ public final class PlayerHudState {
     public boolean staticDirty = true;
 
     public volatile long lastReticleScanMs;
-    public volatile int hideDelayMsHint = GlobalConfig.HIDE_DELAY_MS;
+    public volatile int currentHideDelayMs = GlobalConfig.HIDE_DELAY_MS;
 
     public void reset(int hideDelay) {
         resetHudVisibilityState();
@@ -38,7 +38,7 @@ public final class PlayerHudState {
         heldItem.reset();
         dynamicHudCache.reset();
 
-        hideDelayMsHint = hideDelay;
+        currentHideDelayMs = hideDelay;
     }
 
     public void markStaticHudDirty() {
